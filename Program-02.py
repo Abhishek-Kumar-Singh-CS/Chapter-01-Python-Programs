4e28ecee9b3e34e0480e0b1dcb3c65bb3125a40d
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-09-19 21:23:55 +0530

    Second Program

---
+++ Program-02.py
@@ -0,0 +1,8 @@
+# This is a program to print the area of a square based on the number provided by the user.
+# This is a formula. [A = S * S]
+
+side = int(input("Enter the side value : "))
+
+area = side * side
+
+print("The area of a square is : ",area)
