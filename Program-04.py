d07fa1cf1a25e546e04aad24d13c69b87ba18afa
ABHISHEK KUMAR SINGH <ABHISHEKSILENT0208@GMAIL.COM>
2025-09-21 22:01:10 +0530

    Fourth Program

---
+++ Program-04.py
@@ -0,0 +1,10 @@
+# This is a program to print the greatest number of two numbers provided by the user.
+
+number1 = int(input("Enter the first number : "))
+
+number2 = int(input("Enter the second number : "))
+
+if(number1 >= number2):
+    print("The greatest number of two numbers is : ",number1)
+else:
+    print("The greatest number of two numbers is : ",number2)
